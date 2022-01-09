b97970ba08bfbdd9e701b2e3d1ff3b88d22fc81d
Arslan <arslanamir15@gmail.com>
2022-01-09 16:37:26 -0500

    contract interaction

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -18,7 +18,7 @@ const Main = () => {
 
     //Contract interaction
     const buyPlayer = () => {
-        packPlayersContract.rollDice(currentAccount)
+        packPlayersContract.buy(currentAccount)
     }
 
     return(
@@ -32,7 +32,7 @@ const Main = () => {
                 )}
 
                 <input className="shadow appearance-none border rounded w-half py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Price"/>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4 mt-4">Buy Pack</button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4 mt-4" onClick={buyPlayer}>Buy Pack</button>
             <div>
                 {currentAccount && getBalances(), (
                     <>
