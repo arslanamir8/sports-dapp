cff7961f5a3039f8ff08f1576467c2ec2514e9e9
Arslan <arslanamir15@gmail.com>
2022-01-09 16:25:43 -0500

    Contract Deployed

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,4 +1,4 @@
 import abi from './packPlayers.json'
 
 export const contractABI = abi.abi
-export const contractAddress = '0x4e22b6407bA4ac2CBdB388e09e7e1e25A0E021a1'
+export const contractAddress = '0x31a21ECca62cEEfb62a44Fd7c4493E23C270a675'
