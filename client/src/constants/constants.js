05326f6dcb2fc15860cdba87e451c9b8c8d5024f
Arslan <arslanamir15@gmail.com>
2022-02-05 14:29:02 -0500

    bug

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,4 +1,4 @@
 import abi from './packPlayers.json'
 
 export const contractABI = abi.abi
-export const contractAddress = '0xd345B36FDe5AcAD4385d85586253598489240838'
+export const contractAddress = '0x8005Ab969338f2dD9f91f27c8DD5A19432D17b23'
