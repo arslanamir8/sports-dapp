f9a25606f55ce630af5a2aedc3982cb54283d877
Arslan <arslanamir15@gmail.com>
2022-02-09 17:44:57 -0500

    🙄 refactor and tailwind

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,31 +1,24 @@
+const Player = ({bgColor, position}) => (
+    <div class={`${bgColor} selection:box-border h-50 w-48 grid grid-rows-3 grid-flow-col justify-center`}>
+        <div>{position}</div>
+        <div>Name:</div>
+        <div>Image:</div>
+    </div>
+)
+
 const Landing = () => (
+    //SPLIT EACH BOX INTO A COMPONENT PIECE
     <div>
+        <div>
+            <span class="flex place-content-center">Colosseum</span>
+            <span class="flex place-content-end">Connect Wallet</span>
+        </div>
         <div class="flex justify-center box-border h-80 w-full bg-rose-200">
-            <div class="box-border h-50 w-48 bg-green-200 grid grid-rows-3 grid-flow-col justify-center">
-                <div>PG</div>
-                <div>Name:</div>
-                <div>Image:</div>
-            </div>
-            <div class="box-border h-50 w-48 bg-blue-200 grid grid-rows-3 grid-flow-col justify-center">
-                <p>SG</p>
-                <div>Name:</div>
-                <div>Image:</div>
-            </div>
-            <div class="box-border h-50 w-48 bg-orange-200 grid grid-rows-3 grid-flow-col justify-center">
-                <p>SF</p>
-                <div>Name:</div>
-                <div>Image:</div>
-            </div>
-            <div class="box-border h-50 w-48 bg-yellow-200 grid grid-rows-3 grid-flow-col justify-center">
-                <p>PF</p>
-                <div>Name:</div>
-                <div>Image:</div>
-            </div>
-            <div class="box-border h-50 w-48 bg-purple-200 grid grid-rows-3 grid-flow-col justify-center">
-                <p>C</p>
-                <div>Name:</div>
-                <div>Image:</div>
-            </div>
+            <Player position='PG' bgColor='bg-green-200'/>
+            <Player position='SG' bgColor='bg-blue-200'/>
+            <Player position='SF' bgColor='bg-orange-200'/>
+            <Player position='PF' bgColor='bg-yellow-200'/>
+            <Player position='C' bgColor='bg-purple-200'/>
         </div>
     </div>
     
