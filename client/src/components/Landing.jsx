198cf732f0626089d1788ae2903a3dffbe7c6421
Arslan <arslanamir15@gmail.com>
2022-02-09 15:50:19 -0500

    💩 fuckin css tailwind shittery

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,6 +1,33 @@
 const Landing = () => (
     <div>
-        Hello
+        <div class="flex justify-center box-border h-80 w-full bg-rose-200">
+            <div class="box-border h-50 w-48 bg-green-200 grid grid-rows-3 grid-flow-col justify-center">
+                <div>PG</div>
+                <div>Name:</div>
+                <div>Image:</div>
+            </div>
+            <div class="box-border h-50 w-48 bg-blue-200 grid grid-rows-3 grid-flow-col justify-center">
+                <p>SG</p>
+                <div>Name:</div>
+                <div>Image:</div>
+            </div>
+            <div class="box-border h-50 w-48 bg-orange-200 grid grid-rows-3 grid-flow-col justify-center">
+                <p>SF</p>
+                <div>Name:</div>
+                <div>Image:</div>
+            </div>
+            <div class="box-border h-50 w-48 bg-yellow-200 grid grid-rows-3 grid-flow-col justify-center">
+                <p>PF</p>
+                <div>Name:</div>
+                <div>Image:</div>
+            </div>
+            <div class="box-border h-50 w-48 bg-purple-200 grid grid-rows-3 grid-flow-col justify-center">
+                <p>C</p>
+                <div>Name:</div>
+                <div>Image:</div>
+            </div>
+        </div>
     </div>
+    
 )
 export default Landing
